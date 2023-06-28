dcdfa740a9f6893c4b809d0509a4814f9876e619
davidfontesfelix <davidfontes303@gmail.com>
2023-06-28 14:21:31 -0300

    Code-Splitting

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,65 +1,25 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
+import React, { Suspense, useState } from 'react';
 
-import { createContext, useContext, useState } from 'react';
-import { cloneElement } from 'react';
-import { Children } from 'react';
+const loadComponent = () => import('./lazy-component');
+const LazyComponent = React.lazy(loadComponent);
 
-const s = {
-  style: {
-    fontSize: '50px',
-  },
-};
-
-const TurnOnOffContext = createContext();
-
-const TurnOnOff = ({ children }) => {
-  const [isOn, setIsOn] = useState();
-  const onTurn = () => setIsOn((s) => !s);
+const Home = () => {
+  const [show, setShow] = useState(false);
 
   return (
-    <TurnOnOffContext.Provider value={{ isOn, onTurn }}>
-      {children}
-    </TurnOnOffContext.Provider>
-  );
-};
-
-const TurnedON = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? children : null;
-};
-
-const TurnedOff = ({ children }) => {
-  const { isOn } = useContext(TurnOnOffContext);
-  return isOn ? null : children;
-};
-
-const TurnButton = ({ ...props }) => {
-  const { isOn, onTurn } = useContext(TurnOnOffContext);
-  return (
-    <button onClick={onTurn} {...props}>
-      Turn {isOn ? 'ON' : 'OFF'}
-    </button>
-  );
-};
-
-const P = ({ children }) => <p {...s}>{children}</p>;
-
-export const Home = () => {
-  return (
-    <TurnOnOff>
-      <div>
-        <p>Oi</p>
-        <TurnedON>
-          <P> Aqui as coisa que vão acontecer quando estiver On.</P>
-        </TurnedON>
-
-        <TurnedOff>
-          <P>Aqui as coisas que vão acontecer quando estiver Off.</P>
-        </TurnedOff>
-        <TurnButton {...s} />
-      </div>
-    </TurnOnOff>
+    <div>
+      <p>
+        <button
+          onClick={() => setShow((show) => !show)}
+          onMouseOver={loadComponent}
+        >
+          show {show ? 'LC on screen' : 'LC is off screen'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Carregando...</p>}>
+        {show && <LazyComponent />}
+      </Suspense>
+    </div>
   );
 };
 
